1e9bf5f6755b1520eb961663f19c9a34e0e15a87
Netizen01 <n01@invco.de>
2022-05-09 10:04:01 -0500

    Fix to primaryID on player object, current build returns 0 for any non steam player, this change will return a string of format: "|" correctly for all platforms.

--- SOS/Plugin/GameState.cpp
+++ SOS/Plugin/GameState.cpp
@@ -86,7 +86,7 @@ void SOS::GetIndividualPlayerInfo(json& state, PriWrapper pri)
 
     state["players"][id]["name"] = name;
     state["players"][id]["id"] = id;
-    state["players"][id]["primaryID"] = std::to_string(pri.GetUniqueIdWrapper().GetUID()); // TODO: Needs to be replaced once UniqueIDWrapper gets better sorted out
+    state["players"][id]["primaryID"] = pri.GetUniqueIdWrapper().GetIdString();
     state["players"][id]["shortcut"] = pri.GetSpectatorShortcut();
     state["players"][id]["team"] = pri.GetTeamNum();
     state["players"][id]["score"] = pri.GetMatchScore();
